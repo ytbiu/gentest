4694de773a9f5bd0f4ff790214d023875e2b68bf
yintao <yintao@shuzilm.cn>
2019-10-24 17:38:17 +0800

    .

--- cmd/new.go
+++ cmd/new.go
@@ -23,13 +23,8 @@ import (
 // newCmd represents the new command
 var newCmd = &cobra.Command{
 	Use:   "new",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "gentest is used to create uint test which based on gomock and testify",
+	Long:  `gentest is used to create uint test which based on gomock and testify`,
 	Run: func(cmd *cobra.Command, args []string) {
 		run()
 	},
